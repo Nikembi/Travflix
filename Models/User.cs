2d0b3126b4cd5b6635f4bc7242f02e90b13e73ac
Nikembi Travis <nikembitravis@outlook.com>
2024-11-07 06:40:42 -0600

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Travflix.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
